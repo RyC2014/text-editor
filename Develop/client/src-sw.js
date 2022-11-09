7c86151b6b837be39bee192cae70f1b52cff2da4
RyC2014 <rclingler1986@gmail.com>
2022-11-09 09:21:09 -0500

    added asset cache

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,9 +1,22 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const {
+  offlineFallback,
+  warmStrategyCache
+} = require('workbox-recipes');
+const {
+  CacheFirst
+} = require('workbox-strategies');
+const {
+  registerRoute
+} = require('workbox-routing');
+const {
+  CacheableResponsePlugin
+} = require('workbox-cacheable-response');
+const {
+  ExpirationPlugin
+} = require('workbox-expiration');
+const {
+  precacheAndRoute
+} = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -24,7 +37,28 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(({
+  request
+}) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+
+  ({
+    request
+  }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+
+    cacheName: 'asset-cache',
+    plugins: [
+
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
